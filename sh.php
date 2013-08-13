6730956fddff4060034f9decfb506b79614ab022
Naoki Ikeno <nao@kyo-to.net>
2013-08-13 15:28:41 +0900

    Make the first mode page.

--- sh.php
+++ sh.php
@@ -27,17 +27,41 @@ if( isset( $_GET['mode'] )){
 
 $smarty->display( 'header.tbl' );
 
-
 if ( $mode == 'first' ){
-    echo 'first time.';
+    /*
+      標準画面の表示
+      生成ボタン で org_url、shchar、cutoff、をGETで送る
+    */
+    $smarty->assign( 'org_url', '' );
+    $smarty->assign( 'shchar', '' );
+    $smarty->display( 'first.tbl' );
+
 
 } elseif( $mode == 'gen' ){
+    /*url生成画面
+      shchar の有無チェック
+      shchar あれば
+      shchar の長さチェック
+      エラーで mode=error で戻す
+      shchar なければ、org_url,time より hash で shchar 生成
+
+      org_url,shchar,day を dat へ登録
+
+      shurl の画面を表示
+    */
+
     $org_url = $_GET['org_url'];
-    $shchar = $_GET['shurl'];
-    $cutoff = $_GET['cutoff'];
+    $shchar = $_GET['shchar'];
     print( 'gen mode' );
 
 } elseif ( $mode == 'shurl' ){
+    /*shurl を受け取った
+      GET で shchar を取り出し
+      shchar で dat を検索
+      dat になればエラー画面
+      dat にあれば location  hogeで移動
+    */
+
     $shchar = $_GET['shurl'];
     print( 'shurl mode.' );
 
@@ -50,35 +74,6 @@ if ( $mode == 'first' ){
 }
 
 $smarty->display( 'footer.tbl' );
-
-
-/*
-
-標準画面の表示
- 生成ボタン で org_url、shchar、day、をGETで送る
-
-
-url生成画面
- shchar の有無チェック
-   shchar あれば
-     shchar の長さチェック
-       エラーで mode=error で戻す
-   shchar なければ、org_url,time より hash で shchar 生成
-
- org_url,shchar,day を dat へ登録
-
- shurl の画面を表示
-
-
-shurl を受け取った
- GET で shchar を取り出し
- shchar で dat を検索
-  dat になればエラー画面
-  dat にあれば location  hogeで移動
-
-*/
-
-
 
 
 ?>
